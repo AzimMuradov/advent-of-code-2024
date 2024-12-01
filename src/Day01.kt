f628d789ea07776425636e98584f7526a59a18c6
Azim Muradov <azim.muradov.dev@gmail.com>
2024-12-01 19:59:18 +0300

    Solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,7 +1,27 @@
-fun main() {
-    fun part1(input: List<String>): Int = TODO()
+import kotlin.math.abs
 
-    fun part2(input: List<String>): Int = TODO()
+
+fun main() {
+    fun part1(input: List<String>): Int = input
+        .map { line ->
+            line.toInts("   ").toPair()
+        }
+        .unzip()
+        .let { (xs1, xs2) ->
+            xs1.sorted() zip xs2.sorted()
+        }.sumOf { (x1, x2) ->
+            abs(x1 - x2)
+        }
+
+    fun part2(input: List<String>): Int = input
+        .map { line ->
+            line.toInts("   ").toPair()
+        }
+        .unzip()
+        .let { (xs1, xs2) ->
+            val frequencyMap = xs2.groupingBy { it }.eachCount()
+            xs1.sumOf { x -> x * frequencyMap.getOrDefault(x, 0) }
+        }
 
     val input = readInputLines("day-01-input")
 
--- src/Utils.kt
+++ src/Utils.kt
@@ -15,6 +15,30 @@ fun readInputText(name: String) = Path("src/$name.txt").readText().trim()
 fun readInputLines(name: String) = readInputText(name).lines()
 
 /**
+ * Split string to ints using provided [separator].
+ */
+fun String.toInts(separator: String = " ") = this
+    .split(separator)
+    .map(String::toInt)
+
+/**
+ * Split string to longs using provided [separator].
+ */
+fun String.toLongs(separator: String = " ") = this
+    .split(separator)
+    .map(String::toLong)
+
+/**
+ * Converts list to pair.
+ */
+fun <T> List<T>.toPair() = Pair(this[0], this[1])
+
+/**
+ * Converts list to triple.
+ */
+fun <T> List<T>.toTriple() = Triple(this[0], this[1], this[2])
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
