709b048dff68d3f90aa9daa8948268970fb56334
Azim Muradov <azim.muradov.dev@gmail.com>
2024-12-11 22:11:59 +0300

    Refactor day 11 solution

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,35 +1,32 @@
 fun main() {
-    fun calculateStoneCount(stones: List<Long>, blinksCount: Int): Long {
+    fun calculateStoneCount(stones: List<Long>, blinkCount: Int): Long {
         val mem = mutableMapOf<Pair<Long, Int>, Long>()
 
         val calculateStoneCount = DeepRecursiveFunction<Pair<Long, Int>, Long> { (stone, n) ->
-            val s = stone.toString()
-
-            mem[stone to n] ?: when {
-                n == 0 -> 1L
-
-                stone == 0L -> callRecursive(1L to n - 1)
-
-                s.length % 2 == 0 -> s
-                    .chunked(s.length / 2)
-                    .map(String::toLong)
-                    .sumOf { stonePart -> callRecursive(stonePart to n - 1) }
-
-                else -> callRecursive(stone * 2024 to n - 1)
-            }.also {
-                mem[stone to n] = it
+            mem.getOrPut(stone to n) {
+                if (n != 0) {
+                    val s = stone.toString()
+                    val nextStones = when {
+                        stone == 0L -> listOf(1L)
+                        s.length % 2 == 0 -> s.chunked(size = s.length / 2).map(String::toLong)
+                        else -> listOf(stone * 2024)
+                    }
+                    nextStones.sumOf { stone -> callRecursive(stone to n - 1) }
+                } else {
+                    1L
+                }
             }
         }
 
         return stones.sumOf { stone ->
-            calculateStoneCount(stone to blinksCount)
+            calculateStoneCount(stone to blinkCount)
         }
     }
 
 
-    fun part1(stones: List<Long>): Long = calculateStoneCount(stones, blinksCount = 25)
+    fun part1(stones: List<Long>): Long = calculateStoneCount(stones, blinkCount = 25)
 
-    fun part2(stones: List<Long>): Long = calculateStoneCount(stones, blinksCount = 75)
+    fun part2(stones: List<Long>): Long = calculateStoneCount(stones, blinkCount = 75)
 
 
     val stones = readInputText("day-11-input").toLongs()
